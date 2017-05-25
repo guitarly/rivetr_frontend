530104d5c2dd7f8ce843cf59e1d33c04f8900f26
Jerrica Bobadilla <Jerrica@Jerricas-MacBook-Pro.local>
2017-05-24 18:55:26 -0700

    http reqs for creating new user and new riv

--- public/js/controllers/landing.js
+++ public/js/controllers/landing.js
@@ -1,9 +1,43 @@
+// ================================================
+//               LANDING PAGE CTRL               //
+// ================================================
+
 (function(){
   var app = angular.module('Rivetr');
 
-  // ROUTES CONFIGURATION
-  app.controller('LandingCtrl', function(){
-    this.message = 'hello!'
-  });
+  app.controller('LandingCtrl', ['$http', '$rootScope', function($http, $rootScope){
 
-})();
+    // =========== CONTROLLER VARIABLES ==============
+    const landing = this;
+    if(window.location.href == 'http://localhost:8000/#!/'){
+      var URL = 'http://localhost:3000/';
+    } else {
+      var URL = 'herokulater'
+    };
+
+    // =========== HTTP REQS ==========================
+    // to create a user
+    this.register = function(){
+      $http({
+        method: 'POST',
+        url: URL + 'users',
+        data: this.newUserData
+      }).then(function(result){
+          console.log(result);
+      }.bind(this));
+    };
+
+    // to create a riv
+    this.postRiv = function(){
+      $http({
+        method: 'POST',
+        url: URL + 'rivs',
+        data: this.newRivData
+      }).then(function(result){
+          console.log(result);
+      }.bind(this));
+    };
+
+  }]); // ends controller
+
+})(); // ends closure
